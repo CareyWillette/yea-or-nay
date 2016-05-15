a8609b8ae292fe2a2f6c54c3a9da75dc0eb1bed5
CareyWillette <Carey.Willette@gmail.com>
2016-05-15 22:10:00 +0100

    add seed file, update routes with wildcard route, abstract functions to lib directory

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var db = require('praise.js');
+var parse = require('../lib/body-parser');
+var getResults = require('../lib/results');
+
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('*', function(req, res, next) {
+  res.sendFile('index.html', {
+    root: __dirname + '/../public'
+  })
 });
 
 module.exports = router;
---
+++ seed/create.sql
@@ -0,0 +1,31 @@
+DROP TABLE votes;
+DROP TABLE polls;
+DROP TABLE options;
+
+
+CREATE TABLE polls (
+  id serial primary key,
+  topic text,
+  creator varchar(140),
+  access_code varchar(15),
+  anonymous boolean,
+  is_active boolean default false,
+  vote_ended boolean default false
+);
+
+CREATE TABLE votes (
+  id serial primary key,
+  poll_id int references polls(id) on delete cascade,
+  name varchar(80),
+  vote varchar(140)
+);
+
+CREATE TABLE options (
+  id serial primary key,
+  poll_id int references polls(id) on delete cascade,
+  option_1 varchar(140),
+  option_2 varchar(140),
+  option_3 varchar(140),
+  option_4 varchar(140),
+  option_5 varchar(140)
+);
