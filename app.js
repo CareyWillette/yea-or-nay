059a808811c9161662860abdbade5d97780a16fb
CareyWillette <Carey.Willette@gmail.com>
2016-05-16 23:55:00 +0100

    should have committed sooner. Chartjs needs to be updated

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
+require('dotenv').load();
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-require('dotenv').load();
 var cors = require('express-cors');
 
 
